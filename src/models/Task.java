920d6a72ce42ea129f3165f2c8eadd30d4b8b3b3
jdk_git334 <termination_gfdm@yahoo.co.jp>
2018-11-29 01:59:21 +0900

    Add database files

---
+++ src/models/Task.java
@@ -0,0 +1,26 @@
+package models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
